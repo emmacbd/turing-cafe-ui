9aeba47af72fcec20004e8f4b6151158ae314de9
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-05-25 11:11:31 -0500

    Add handlechange, styling

--- src/App/Form.js
+++ src/App/Form.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react"
+import "./Form.css"
+
 
 class Form extends Component {
   constructor(){
@@ -8,17 +10,50 @@ class Form extends Component {
       name: '',
       date: '',
       time: '',
-      number:
+      number: undefined,
     }
   }
 
+handleChange = event => {
+  this.setState({[event.target.name]:event.target.value})
+}
+
   render(){
     return (
-      <form>
-        <input> </input>
-        <input> </input>
-        <input> </input>
-        <input> </input>
+      <form className="form">
+        <input
+          type='text'
+          placeholder='Name'
+          name='name'
+          value={this.state.name}
+          onChange={event => this.handleChange(event)}
+        />
+        <input
+          type='date'
+          placeholder='Date (mm/dd)'
+          name='datetime-local'
+          value={this.state.date}
+          onChange={event => this.handleChange(event)}
+          />
+
+         <input
+          type='text'
+          placeholder='Time'
+          name='time'
+          value={this.state.time}
+          onChange={event => this.handleChange(event)}
+          />
+
+        <input
+          type='number'
+          placeholder='Number of Guests'
+          min="1"
+          name='guests'
+          value={this.state.number}
+          onChange={event => this.handleChange(event)}
+          />
+
+        <button className="resy-button">MAKE RESERVATION</button>
       </form>
     )
   }
